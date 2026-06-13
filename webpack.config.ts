f1d25429439c2645e9d866606c5a5f2a0b51efbe
Artyushenko Daniil <foxones80@gmail.com>
2026-06-13 23:01:55 +0600

    ci config

--- webpack.config.ts
+++ webpack.config.ts
@@ -12,7 +12,7 @@ export default (env: EnvOptions) => {
     const paths: BuildPaths = {
         entry: path.resolve(__dirname, "src", "index.tsx"),
         output: path.resolve(__dirname, "build"),
-        html: path.resolve(__dirname, "public", "index.html"),
+        html: path.resolve(__dirname, "public/index.html"),
         src: path.resolve(__dirname, "src"),
     };
 
